82abfbc3071be6dd201cfcb6d156a47fa3b3e756
Erich Reitz <erich.reitz@protonmail.com>
2023-11-16 22:38:00 -0600

    test mult

--- tests/test.nim
+++ tests/test.nim
@@ -18,13 +18,37 @@ proc runTestFile(testname: string): string =
   let (output, exitcode) = execTestCaptureOutput(testname)
   assert exitcode == 0
   return output
-  
+
+proc annotateStringDiff(original: string, modified: string): string =
+    var result = ""
+    var i = 0
+    var j = 0
+
+    while i < original.len or j < modified.len:
+        if i < original.len and j < modified.len and original[i] == modified[j]:
+            # No change in this character
+            result.add(original[i])
+            inc(i)
+            inc(j)
+        elif i < original.len and (j >= modified.len or original[i] notin modified):
+            # Character deleted from original
+            result.add("-" & original[i])
+            inc(i)
+        elif j < modified.len:
+            # Character added in modified
+            result.add("+" & modified[j])
+            inc(j)
+
+    return result
+
+
 proc runTest(testname: string): bool =
   let output = runTestFile(testname)
   let expectedOutput = expectedTestOutput(testname)
 
-  return output == expectedOutput
-
+  result = output == expectedOutput
+  if result == false:
+    echo annotateStringDiff(output, expectedOutput)
 
 suite "integration tests":
   # compile main program once before executing tests
@@ -50,3 +74,6 @@ suite "integration tests":
 
   test "minus":
     check runTest("minus")
+  
+  test "mult":
+    check runTest("mult")
